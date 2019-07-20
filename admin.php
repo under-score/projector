ce7e91c028ccde4440e662876df91de6e2877f94
under-score <2796454+under-score@users.noreply.github.com>
2019-07-20 15:48:25 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -11,7 +11,7 @@
 <meta http-equiv="Expires" content="0" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 <link rel="stylesheet" type="text/css" href="projector.css" />
 
